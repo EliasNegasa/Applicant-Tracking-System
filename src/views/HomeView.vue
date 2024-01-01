603c7bf999bf02dd51e620f5a335222bd9446ee3
= <=>
2024-01-01 13:34:25 +0300

    home view temp

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,14 @@
-<script setup lang="ts">
-</script>
-
 <template>
-  <main>Home View</main>
+  <div>
+    <h2 class="text-lg font-bold mb-4 ml-4">Home</h2>
+  </div>
+  <div class="flex gap-6">
+    <div class="w-full shadow-md shadow-gray-100">
+      <DashboardCard />
+    </div>
+  </div>
 </template>
+
+<script setup>
+import DashboardCard from '../components/home/DashboardCard.vue'
+</script>
